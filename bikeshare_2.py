44e7ee4ee758d319079325b1b415fafe41c17a71
Bekono Ottou Justin Protais <bekonoottou@gmail.com>
2021-10-01 02:04:11 +0100

    docs: update the docstring of the user_stats(df) function

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -185,7 +185,7 @@ def trip_duration_stats(df):
 
 
 def user_stats(df):
-    """Displays statistics on bikeshare users."""
+    """Provide statistical data on bikeshare users."""
 
     print('\nCalculating User Stats...\n')
     start_time = time.time()
